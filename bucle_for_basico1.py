548359e3b3d9c6b98b1cbdfdf8458b08f2beab34
carosantis <110264799+carosantis@users.noreply.github.com>
2022-09-23 17:16:49 -0300

    Add files via upload

---
+++ bucle_for_basico1.py
@@ -0,0 +1,38 @@
+i = 0
+while (i<= 150):
+    print (i)
+    i = i+1
+
+for x in range (0, 1000):
+    if x%5==0:
+        print (x)
+
+for x in range (0, 100):
+    if x%10==0:
+        print ("Coding Dojo")
+    elif  x%5==0:
+        print ("Coding")
+    else:
+         print (x) 
+
+suma = 0
+for x in range (0, 500000):    
+    if x % 2 != 0:
+        suma = suma + x
+print (suma) 
+
+z = 2018
+while z > 0:
+    print (z)
+    z = z - 4
+
+
+lowNum = 6 
+highNum = 100
+mult = 7
+print (lowNum)
+for x in range (lowNum, highNum):    
+    if x % mult == 0:
+        print (x)
+print (highNum)
+    
